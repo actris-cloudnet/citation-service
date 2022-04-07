d3d49506112f6993452c0629f364c54b9d4463ee
Simo Tukiainen <simo.tukiainen@fmi.fi>
2022-04-07 16:13:26 +0300

    Update setup.py

--- service/setup.py
+++ service/setup.py
@@ -2,6 +2,10 @@ from setuptools import find_packages, setup
 
 setup(
     name="citation-service",
-    install_requires=["httpx", "fastapi", "uvicorn", "html-sanitizer"],
+    setup_requires="wheel",
+    install_requires=["h11==0.12.0", "httpx", "fastapi", "uvicorn", "html-sanitizer"],
+    extras_require={
+        "dev": ["pylint", "pre-commit"],
+    },
     packages=find_packages(),
 )
