03abe22ec3b7ec2fbfe1aa666ccd4b1842b1f345
Tuomas Siipola <tuomas.siipola@fmi.fi>
2023-08-31 17:20:31 +0300

    Lock FastAPI and Pydantic versions

--- service/setup.py
+++ service/setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="citation-service",
     setup_requires="wheel",
-    install_requires=["h11==0.12.0", "httpx", "fastapi", "uvicorn", "html-sanitizer"],
+    install_requires=["h11==0.12.0", "httpx", "fastapi>=0.103.0,<0.104.0", "pydantic>=1.2.0,<2.0.0", "uvicorn", "html-sanitizer"],
     extras_require={
         "dev": ["pylint", "pre-commit"],
     },
